3f5052c712acb07937ac362aa6988ac4b4667129
Javi Filella <txusko@gmail.com>
2018-02-22 16:39:59 +0100

    Read OCR data files

--- ocr.rb
+++ ocr.rb
@@ -33,57 +33,66 @@ NUMBERS[9]  = " _ " +
               " _|"
 
 
-def read_line
-    line = gets
-    line ? line[0...(CHAR_LEN * NUM_LEN)] : nil
+def scan_number
+  lines = [read_line(gets), read_line(gets), read_line(gets)]
+  return nill unless lines[0] && lines[1] && lines[2]
+  gets #4rth line (enter)
+  recover_number(lines)
 end
 
-def scan_number
-    line1 = read_line
-    line2 = read_line
-    line3 = read_line
-    return nill unless line1 && line2 && line3
+def scan_file(filename)
+  lines = []
+  File.open(filename).each do |line|
+    puts line
+    lines.push(read_line(line))
+    if lines.length >= 4
+      temp_num = recover_number(lines)
+      show_number(temp_num)
+      lines = []
+    end
+  end
+end
 
-    lines = [line1, line2, line3]
-    gets #4rth line (enter)
-    recover_number(lines)
+def read_line(line)
+  line ? line[0...(CHAR_LEN * NUM_LEN)].ljust(CHAR_LEN * NUM_LEN) : nil
 end
 
 def recover_number(lines)
-    part1 = lines[0].scan(/.../)
-    part2 = lines[1].scan(/.../)
-    part3 = lines[2].scan(/.../)
-    numbers = part1.zip(part2, part3).map { |part| part.join }
-    get_number(numbers)
+  part1 = lines[0].scan(/.../)
+  part2 = lines[1].scan(/.../)
+  part3 = lines[2].scan(/.../)
+  numbers = part1.zip(part2, part3).map { |part| part.join }
+  get_number(numbers)
 end
 
 def get_number(numbers)
-    hash = Hash[NUMBERS.map.with_index.to_a]
-    numbers.map{ |i| hash[i] ? hash[i].to_s : "?" }.join
+  hash = Hash[NUMBERS.map.with_index.to_a]
+  numbers.map{ |i| hash[i] ? hash[i].to_s : "?" }.join
 end
 
 def checksum?(number)
-    tot = 0;
-    number.reverse.chars.each_with_index { |e, index|
-      tot += (index + 1) * e.to_i
-    }
-    (tot % 11) == 0
+  tot = 0;
+  number.reverse.chars.each_with_index { |e, index| tot += (index + 1) * e.to_i }
+  (tot % 11) == 0
 end
 
 def show_number(number)
-    print number
-    if number =~ /[?]/
-        puts " ILL"
+  if number =~ /[?]/
+    puts "#{number} ILL"
+  else
+    if !checksum?(number)
+      puts "#{number} ERR"
     else
-        if checksum?(number)
-            puts " NIL"
-        end
+      puts number
     end
-    puts
+  end
 end
 
 while temp_num = scan_number
-    show_number(temp_num)
+   show_number(temp_num)
 end
+
+# scan_file('./data/data1.txt')
+# scan_file('./data/data2.txt')
 
 #show_number(scan_number)
