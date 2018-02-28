627c2f043fc5e381bfb1bb8c4ebf22ce53ea0d9c
Miquel Oliete <miquel.oliete@sage.com>
2018-02-28 13:01:43 +0100

    Some suggestions 1

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -1,43 +1,44 @@
 CHAR_LEN = 3
 NUM_LEN = 9
-NUMBERS = Array.new(10)
-NUMBERS[0]  = ' _ ' \
-              '| |' \
-              '|_|'
-NUMBERS[1]  = '   ' \
-              '  |' \
-              '  |'
-NUMBERS[2]  = ' _ ' \
-              ' _|' \
-              '|_ '
-NUMBERS[3]  = ' _ ' \
-              ' _|' \
-              ' _|'
-NUMBERS[4]  = '   ' \
-              '|_|' \
-              '  |'
-NUMBERS[5]  = ' _ ' \
-              '|_ ' \
-              ' _|'
-NUMBERS[6]  = ' _ ' \
-              '|_ ' \
-              '|_|'
-NUMBERS[7]  = ' _ ' \
-              '  |' \
-              '  |'
-NUMBERS[8]  = ' _ ' \
-              '|_|' \
-              '|_|'
-NUMBERS[9]  = ' _ ' \
-              '|_|' \
-              ' _|'
+NUMBERS = [
+  ' _ ' \
+  '| |' \
+  '|_|',
+  '   ' \
+  '  |' \
+  '  |',
+  ' _ ' \
+  ' _|' \
+  '|_ ',
+  ' _ ' \
+  ' _|' \
+  ' _|',
+  '   ' \
+  '|_|' \
+  '  |',
+  ' _ ' \
+  '|_ ' \
+  ' _|',
+  ' _ ' \
+  '|_ ' \
+  '|_|',
+  ' _ ' \
+  '  |' \
+  '  |',
+  ' _ ' \
+  '|_|' \
+  '|_|',
+  ' _ ' \
+  '|_|' \
+  ' _|'
+].freeze
 
 module OCR
   # OCR Reader class
   class OCR
     def scan_number
       lines = [read_line(gets), read_line(gets), read_line(gets)]
-      return nill unless lines[0] && lines[1] && lines[2]
+      return nil unless lines[0] && lines[1] && lines[2]
       gets # 4rth line (enter)
       recover_number(lines)
     end
@@ -45,7 +46,7 @@ module OCR
     def scan_file(filename)
       results = []
       lines = []
-      File.open(filename).each do |line|
+      File.open(filename).each_line do |line|
         lines.push(line)
         next unless lines.length >= 4
         results.push(recover_number(lines))
@@ -67,16 +68,18 @@ module OCR
     end
 
     def get_number(numbers)
-      hash = Hash[NUMBERS.map.with_index.to_a]
+      hash = NUMBERS.map.with_index.to_a.to_h
       numbers.map { |i| hash[i] ? hash[i].to_s : '?' }.join
     end
 
     def checksum?(number)
       tot = 0
-      number = number.to_s.rjust(9, '0')
-      number.reverse.chars.each_with_index do |e, index|
-        tot += (index + 1) * e.to_i
+      processed_number = number.to_s.rjust(NUM_LEN, '0').reverse
+      processed_number.chars.each_with_index do |element, i|
+        tot += (i + 1) * element.to_i
       end
+
+      # Why 11 (maybe include a constant that explains what this 11 is)?
       (tot % 11).zero?
     end
 
@@ -84,57 +87,51 @@ module OCR
       alts = []
       number = get_account_with_question_marks(number)
       if !checksum?(number)
-        equivalences = []
-        equivalences[0] = [8]
-        equivalences[1] = [7]
-        equivalences[3] = [9]
-        equivalences[5] = [9, 6]
-        equivalences[6] = [8, 5]
-        equivalences[7] = [1]
-        equivalences[9] = [8, 5, 3]
-        equivalences[8] = [9, 6, 0]
-        equivalences.each_with_index do |num, index|
+        define_equivalences.each_with_index do |num, index|
           next if num.nil?
-          num.each do |to_num|
-            cp_num = number.dup
+          num.each do |elem|
             next unless number.include?(index.to_s)
-            get_num_alternatives(number, index, cp_num,
-                                 to_num.to_s, alts)
+            get_num_alternatives(number, index,
+                                 elem.to_s, alts)
           end
         end
       else
         alts.push(number) unless alts.include?(number)
       end
+
       show_alternatives(number, alts)
     end
 
+    def define_equivalences
+      [
+        [8], [7], nil, [9], nil, [9, 6], [8, 5], [1], [9, 6, 0], [8, 5, 3]
+      ]
+    end
+
     def show_alternatives(number, alts)
-      alts.sort!
       if alts.length > 1
-        "#{number} AMB ['" + alts.join("', '") + "']"
+        "#{number} AMB ['" + alts.sort.join("', '") + "']"
       else
         check_number(alts[0])
       end
     end
 
-    def get_num_alternatives(number, index, cp_num, to_num, alts)
-      realindex = 0
-      cp_num.split('').each do
+    def get_num_alternatives(number, index, elem, alts)
+      number.split('').each_with_index do |_elem, real_index|
+        next unless number[real_index] == index.to_s
+
         cp_num = number.dup
-        if cp_num[realindex] == index.to_s
-          cp_num[realindex] = to_num
-          alts.push(cp_num) if !alts.include?(cp_num) && checksum?(cp_num)
-        end
-        realindex += 1
+        cp_num[real_index] = elem
+        alts.push(cp_num) if !alts.include?(cp_num) && checksum?(cp_num)
       end
     end
 
     def get_account_with_question_marks(number)
       if number =~ /[?]/
-        (0..9).each do |i|
-          cp_number = number.dup
-          cp_number = cp_number.sub('?', i.to_s)
-          return cp_number if checksum?(cp_number)
+        (0..NUM_LEN).each do |i|
+          # check difference between method & method!
+          addressed_number = number.sub('?', i.to_s)
+          return addressed_number if checksum?(addressed_number)
         end
       end
       number
